c48ea3483bb9f0b501a3080c4f39ff2eb2671c3d
hongkvu <hong.vu@sjsu.edu>
2022-11-26 16:35:54 -0800

    Update queries

---
+++ database schema/MySQL-queries.sql
@@ -0,0 +1,24 @@
+# When the request sends by a users, the request table will be updated, the request id will automated apply increment 
+INSERT INTO request (requestID, reason, status, URL)
+VALUES(1009,'','IN PROCESS','');
+
+# If the request is approved, the reponse will be updated and restriced website will be updated
+INSERT INTO response (responseID, policyID, message)
+VALUES('2009','','the request has been aprroved');
+
+DELETE FROM restrictedwebsites
+WHERE url = "www.google.com";
+
+UPDATE request
+SET status = "Approved"
+WHERE requestID = '1009';
+
+# IF the request is denied
+UPDATE request
+SET status = "Decined"
+WHERE requestID = '1009';
+
+
+
+  
+
